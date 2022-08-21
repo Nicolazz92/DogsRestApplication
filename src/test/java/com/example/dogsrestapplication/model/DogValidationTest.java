2574f3de79920e8c2cc66208907c03ac54cc4f7d
sbt-velikokhatko-nn <Nik403512>
2022-08-21 21:54:07 +0300

    Добавил тест hibernate валидатора

--- src/test/java/com/example/dogsrestapplication/model/DogValidationTest.java
+++ src/test/java/com/example/dogsrestapplication/model/DogValidationTest.java
@@ -1,8 +1,45 @@
 package com.example.dogsrestapplication.model;
 
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.time.LocalDate;
+import java.util.Set;
+
+import static com.example.dogsrestapplication.TestUtils.newDogFactoryMethod;
+import static com.example.dogsrestapplication.TestUtils.newNonValidatedDogFactoryMethod;
+import static org.testng.Assert.assertTrue;
 
 @Test
 public class DogValidationTest {
-    //TODO
+
+    private static Validator validator;
+
+    @BeforeClass
+    public static void setUp() {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
+    @Test
+    public void correctDogTest() {
+        Dog correctDog = newDogFactoryMethod();
+        Set<ConstraintViolation<Dog>> validate = validator.validate(correctDog);
+        assertTrue(validate.isEmpty());
+    }
+
+    @Test
+    public void incorrectDogTest() {
+        Dog incorrectDog = newNonValidatedDogFactoryMethod();
+        LocalDate dateOfBirth = incorrectDog.getDateOfBirth();
+
+        Set<ConstraintViolation<Dog>> validate = validator.validate(incorrectDog);
+
+        assertTrue(validate.stream().anyMatch(v -> "name size 'x' is not between 2 and 50".equals(v.getMessage())));
+        assertTrue(validate.stream().anyMatch(v -> "height '1000' is greater then 200".equals(v.getMessage())));
+        assertTrue(validate.stream().anyMatch(v -> "weight '1000' is greater then 100".equals(v.getMessage())));
+        assertTrue(validate.stream().anyMatch(v -> String.format("dateOfBirth '%s' is future", dateOfBirth).equals(v.getMessage())));
+    }
 }
